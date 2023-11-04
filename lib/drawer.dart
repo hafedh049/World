ddc7a1a464c27ad28ce14243be51bbab6f644fd9
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-04 10:42:20 +0100

    A

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -11,56 +11,89 @@ class DDrawer extends StatefulWidget {
 
 class _DDrawerState extends State<DDrawer> {
   bool _cocktailShaker = false;
-  final Widget _logo = Image.asset("assets/w.png", width: 100, height: 100);
+  bool _me = false;
+  final Widget _logo = Image.asset("assets/w.png", width: 150, height: 150);
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
+          child: Stack(
+            alignment: AlignmentDirectional.bottomStart,
             children: <Widget>[
-              Center(
-                child: StatefulBuilder(
-                  builder: (BuildContext context, void Function(void Function()) _) {
-                    return InkWell(
-                      hoverColor: transparent,
-                      splashColor: transparent,
-                      highlightColor: transparent,
-                      onTap: () => true,
-                      onHover: (bool value) => _(() => _cocktailShaker = value),
-                      child: _cocktailShaker ? _logo.animate().shakeX() : _logo,
-                    );
-                  },
-                ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Center(
+                    child: StatefulBuilder(
+                      builder: (BuildContext context, void Function(void Function()) _) {
+                        return InkWell(
+                          hoverColor: transparent,
+                          splashColor: transparent,
+                          highlightColor: transparent,
+                          onTap: () => true,
+                          onHover: (bool value) => _(() => _cocktailShaker = value),
+                          child: _cocktailShaker ? _logo.animate().shakeX() : _logo,
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  for (final Map<String, dynamic> entry in menu)
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16, bottom: 16),
+                      child: StatefulBuilder(
+                        builder: (BuildContext context, void Function(void Function()) _) {
+                          return InkWell(
+                            onTap: () => true,
+                            onHover: (bool value) => _(() => entry["state"] = value),
+                            splashColor: transparent,
+                            hoverColor: transparent,
+                            highlightColor: transparent,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Row(mainAxisSize: MainAxisSize.min, children: <Widget>[Icon(entry["icon"], size: 15, color: entry["state"] ? blue : null), const SizedBox(width: 10), Text(entry["item"], style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: entry["state"] ? blue : null))]),
+                                const SizedBox(height: 5),
+                                AnimatedContainer(duration: 700.ms, height: .9, width: entry["state"] ? entry["item"].length * 12.0 + 5 : 0, color: white),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                ],
               ),
-              const SizedBox(height: 30),
-              for (final Map<String, dynamic> entry in menu)
-                Padding(
-                  padding: const EdgeInsets.only(left: 16, bottom: 16),
-                  child: StatefulBuilder(
-                    builder: (BuildContext context, void Function(void Function()) _) {
-                      return InkWell(
-                        onTap: () => true,
-                        onHover: (bool value) => _(() => entry["state"] = value),
-                        splashColor: transparent,
-                        hoverColor: transparent,
-                        highlightColor: transparent,
-                        child: Column(
+              StatefulBuilder(
+                builder: (BuildContext context, void Function(void Function()) _) {
+                  return InkWell(
+                    onTap: () => true,
+                    onHover: (bool value) => _(() => _me = value),
+                    splashColor: transparent,
+                    hoverColor: transparent,
+                    highlightColor: transparent,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
                           mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Row(mainAxisSize: MainAxisSize.min, children: <Widget>[Icon(entry["icon"], size: 15, color: entry["state"] ? blue : null), const SizedBox(width: 10), Text(entry["item"], style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: entry["state"] ? blue : null))]),
-                            const SizedBox(height: 5),
-                            AnimatedContainer(duration: 700.ms, height: .9, width: entry["state"] ? entry["item"].length * 12.0 + 5 : 0, color: white),
+                            const CircleAvatar(radius: 15, backgroundColor: blue, child: CircleAvatar(backgroundImage: AssetImage("assets/me.png"))),
+                            const SizedBox(width: 10),
+                            Text("Powered by me.", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: _me ? blue : null)),
                           ],
                         ),
-                      );
-                    },
-                  ),
-                ),
+                        const SizedBox(height: 5),
+                        AnimatedContainer(duration: 700.ms, height: .9, width: _me ? 15 + 5 : 0, color: white),
+                      ],
+                    ),
+                  );
+                },
+              )
             ],
           ),
         ),
